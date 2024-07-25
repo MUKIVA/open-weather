72b9e2daa2d22f3f312fb28f760d3a2e5acdf9ce
mukiva <mukiva2261@gmail.com>
2024-07-25 08:38:42 +0300

    Перенес удаление стандартного стартера WorkManager в главный манифест & повысил версию сборки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
 
     defaultConfig {
         applicationId = "com.github.mukiva.openweather"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0-RC02"
 
         resourceConfigurations += setOf("ru", "en")
 
